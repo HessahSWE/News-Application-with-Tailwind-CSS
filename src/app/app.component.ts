dbe5b85bae3b508a29ebda5f4af4e33179cfd296
HessahSWE <hissahalhussain@gmail.com>
2022-10-12 00:22:18 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
 import { AfterViewInit, Component, Injectable, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/layout'; 
+import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-@Injectable()
+
 export class AppComponent implements AfterViewInit {
   title = 'News-Application-with-Tailwind-CSS';
   @ViewChild(MatSidenav) sideNav!: MatSidenav;
   constructor(private observer: BreakpointObserver) {
-  
+
   }
 
   ngAfterViewInit(): void {
     this.sideNav.opened = true;
     this.observer.observe(['(max-width :787px)']).subscribe((res) => {
-      if (res.matches) {
+      if (res?.matches) {
         this.sideNav.mode = 'over';
         this.sideNav.close();
       } else {
